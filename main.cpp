4ca9ddc6489a6c986c577a8dad5faee05dab4cba
wdc <129733701+wdcww@users.noreply.github.com>
2024-03-30 15:33:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,61 +8,6 @@ using namespace std;
 
 
 
-void test01()
-{
-    speechManager s; //创建一个管理类的对象
-    while(true)
-    {  
-            cout<<endl;
-            s.show_menu();
-            int a;
-            cout<<"请输入你的数字选择";
-            cin>>a;
-            cout<<"你选择了功能（"<<a<<")"<<endl;
-              switch(a)
-                        { 
-                            case 1:{
-                            cout<<endl;
-                            s.startSpeech(); //开始演讲比赛
-                            cout<<endl;
-                            system("pause");
-                            } break;
-
-                            case 2:{
-                            cout<<endl;
-                                      //查看往届记录
-                            cout<<endl;
-                            system("pause");
-                            } break;
-
-                            case 3:{
-                            cout<<endl; 
-                                       //清空比赛记录
-                            cout<<endl;
-                            system("pause");
-                            } break;
-
-                            case 0:{
-                            cout<<endl; 
-                            s.exit_speechManager();// 退出
-                            cout<<endl;
-                            } break;
-
-                            default:break;   
-                        }
-                
-                if(a!=0 && a!=1 && a!=2 && a!=3)
-               {
-                cout<<endl;
-                cout<<"请输人有效的数字"<<endl;
-                system("pause");
-                cout<<endl;
-               }
-         system("cls");
-
-    }
-}
-
 void test02()
 {
     speechManager spmanager; //创建一个管理类的对象
@@ -116,28 +61,25 @@ void test02()
 
     }
 
-//  ////a=0, 从 while(true) 中出来, test02()就正常结束了,再打印一下下面的东西
-
-//     map<int,Speaker>::iterator it;
-//     for(it = spmanager.m_Speaker.begin(); it!=spmanager.m_Speaker.end(); it++)
-//     {
-//        cout<<"选手编号:"<<it->first  //it是map<int,Speaker>容器的迭代器,it->first是int
-//            <<"  姓名:"<<it->second.m_Name  //it->second是Speaker,然后Speaker有两个成员,m_Name,m_Score
-//            <<"  第一轮成绩:"<<it->second.m_Score[0]
-//            <<"  第二轮成绩:"<<it->second.m_Score[1]<<endl;
-//         cout<<endl;
-//     }
-
+        //  ////a=0, 从 while(true) 中出来, test02()就正常结束了,再打印一下下面的东西
+        //     map<int,Speaker>::iterator it;
+        //     for(it = spmanager.m_Speaker.begin(); it!=spmanager.m_Speaker.end(); it++)
+        //     {
+        //        cout<<"选手编号:"<<it->first  //it是map<int,Speaker>容器的迭代器,it->first是int
+        //            <<"  姓名:"<<it->second.m_Name  //it->second是Speaker,然后Speaker有两个成员,m_Name,m_Score
+        //            <<"  第一轮成绩:"<<it->second.m_Score[0]
+        //            <<"  第二轮成绩:"<<it->second.m_Score[1]<<endl;
+        //         cout<<endl;
+        //     }
 
 }
+
 
 
 int main()
 {   
     //为了能够看到析构函数的调用,把主体写在其他的函数中
-    // test01();//这个是 s.exit_speechManager(); 的exit(0)退出0
-    test02();//这个是函数执行完的正常结束
-
+    test02();
 
     system("pause");
     return 0;
